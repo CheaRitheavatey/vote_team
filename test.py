0a75c0fa3b0cdcfd7fabdfc74c85d51ff4cf7f47
Chea Ritheavatey <2023447chea@aupp.edu.kh>
2025-10-22 22:06:15 +0200

    Update test.py

--- test.py
+++ test.py
@@ -147,10 +147,66 @@ def interactive_vote():
     submit_answer(enter_code, block_id, question_id, choice)
 
 # get result
-def get_survey_results(enter_code):
-    response = requests.get(f"{BASE_URL}/analysis/{enter_code}/blocks", headers=headers)
+def get_survey_results(enter_code, block_id=0, question_id=0):
+    response = requests.get(f"{BASE_URL}/analysis/{enter_code}/blocks/{block_id}/questions/{question_id}", headers=headers)
     print("Results status:", response.status_code)
-    print(response.text)
+    
+    if response.status_code != 200:
+        print("cannot fetch result:" , response.text)
+        return
+    
+    data = response.json()
+    events = data.get("events", [])
+    
+    if not events:
+        print("Not enough responses yet.")
+        return
+
+    # count vote
+    count = {}
+    for event in events:
+        content = event.get("content", {})
+        answer = (
+            content.get("answer", {})
+            .get("0", {})
+            .get("0", [{}])[0]
+            .get("answer")
+        )
+        if answer is not None:
+            if answer in count:
+                current_vote = count[answer]
+                new_vote = current_vote + 1
+                count[answer] = new_vote
+            else:
+                count[answer] = 1
+            # count[answer] = count.get(answer, 0) + 1
+            # print(count)
+
+    # fetch question to display info 
+    q = fetch_question(enter_code, block_id, question_id)
+    
+    option = [v["DE"] for k, v in q["config"]["options"].items()]
+    question_text = q["question"]["DE"]
+
+    print(f"\nResults for Survey {enter_code}")
+    print(f"Question: {question_text}")
+    print("-----------------------------------")
+
+    total_votes = 0
+    for idx, opt_text in enumerate(option):
+        votes = count.get(str(idx), 0) # if not found return 0
+        total_votes += votes
+        # print("votes: " , votes)
+        # print("total: " , total_votes)
+        
+        # want to display option : n vote
+        print(f"{opt_text}: {votes} votes")
+
+    print("-----------------------------------")
+    print(f"Total responses: {total_votes}")
+
+
+    # print(response.text)
 
 
 if __name__ == "__main__":
@@ -170,7 +226,7 @@ if __name__ == "__main__":
             interactive_vote()
         elif choice == "4":
             code = input("Enter survey code: ")
-            get_survey_results(code)
+            get_survey_results(code,block_id=0,question_id=0)
         elif choice == "5":
             break
         else:
